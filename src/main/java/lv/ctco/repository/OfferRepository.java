08b0934b311eddb5c34062369867cb24997e1635
Nikita <n.smirnovs.0612@gmail.com>
2016-07-11 14:46:59 +0300

    offer controller finished

--- src/main/java/lv/ctco/repository/OfferRepository.java
+++ src/main/java/lv/ctco/repository/OfferRepository.java
@@ -2,8 +2,13 @@ package lv.ctco.repository;
 
 
 import lv.ctco.Entities.Offer;
+import lv.ctco.Entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface OfferRepository extends JpaRepository<Offer,Integer> {
-
+    @Query("select o from Offer o where o.user = ?1")
+    List<Offer> getByDriverID(User driver);
 }
---
+++ src/test/java/lv/ctco/controllers/OfferControllerTest.java
@@ -0,0 +1,33 @@
+package lv.ctco.controllers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OfferControllerTest {
+
+    @Test
+    public void testGetOffers() throws Exception {
+
+    }
+
+    @Test
+    public void testGetOffersById() throws Exception {
+
+    }
+
+    @Test
+    public void testGetOffersByDriverID() throws Exception {
+
+    }
+
+    @Test
+    public void testPostOffer() throws Exception {
+
+    }
+
+    @Test
+    public void testDeleteOfferById() throws Exception {
+
+    }
+}
