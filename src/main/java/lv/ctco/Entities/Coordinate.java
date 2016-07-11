d5165b15f2ce95523bcb3d6292fd69099d1534df
Vadim Rozhkalns <vadim.rozhkalns@gmail.com>
2016-07-11 12:17:20 +0300

    added

--- src/main/java/lv/ctco/Entities/Coordinate.java
+++ src/main/java/lv/ctco/Entities/Coordinate.java
@@ -1,8 +1,20 @@
 package lv.ctco.Entities;
 
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
 public class Coordinate {
 
+    @Column(name = "ORIGIN")
     private double latitude;
+    @Column(name = "DESTINATION")
     private double longitude;
+
+    public Double distance() {
+
+        return null;
+    }
 
 }
