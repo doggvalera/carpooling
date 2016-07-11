7ca78f64e6ca3cabfd2c3d0d199eb6f4c955b2d4
Valera Zemlanikin <doggvalera@gmail.com>
2016-07-11 14:11:04 +0300

    Remove duplicated directory

---
+++ src/test/java/lv/ctco/controllers/RideControllerTest.java
@@ -0,0 +1,23 @@
+package lv.ctco.controllers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RideControllerTest {
+
+    @Test
+    public void testShowAllRides() throws Exception {
+
+    }
+
+    @Test
+    public void testShowRidesByDriver() throws Exception {
+
+    }
+
+    @Test
+    public void testShowRidesPassenger() throws Exception {
+
+    }
+}
