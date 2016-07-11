429f51ce4d8df7ac51e976309096f39895e23c74
Nikita <n.smirnovs.0612@gmail.com>
2016-07-11 14:05:05 +0300

    ride.html , offer controller

--- src/main/java/lv/ctco/controllers/OfferController.java
+++ src/main/java/lv/ctco/controllers/OfferController.java
@@ -1,4 +1,41 @@
 package lv.ctco.controllers;
 
+import lv.ctco.repository.OfferRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(name = "/rides/offer")
 public class OfferController {
+
+    @Autowired
+    OfferRepository offerRepository;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<?> getOffers() {
+        offerRepository.findAll();
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @RequestMapping(path = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> getOffersById(@PathVariable("id") int id) {
+        offerRepository.findOne(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @RequestMapping(path = "/bydriver/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> getOffersByDriverID(@PathVariable("id") int id) {
+        offerRepository.findOne(id);
+
+//        List<Offer> driversByID = new ArrayList<>();
+//        for (int i = 0; i < offerRepository.findAll().size(); i++) {
+//            if (offerRepository.findAll().get(i).getId() == id) {
+//                driversByID.add(offerRepository.findOne(i));
+//            }
+//        }
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 }
