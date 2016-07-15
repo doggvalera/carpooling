1d6e34a30bb26dd01587e525f4563eb1be06db6d
Nelosh <asakura.jo@gmail.com>
2016-07-15 12:02:35 +0300

    fixe #33, replaced fetches with ajax

--- src/main/java/lv/ctco/config/SecurityConfiguration.java
+++ src/main/java/lv/ctco/config/SecurityConfiguration.java
@@ -26,9 +26,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeRequests()
-                .antMatchers("/**").authenticated().and().formLogin()
-                .loginPage("/index.html")
-                .permitAll().and()
+                .antMatchers("/**").fullyAuthenticated().and().formLogin()
+                .loginPage("/index.html").permitAll().and()
                 .httpBasic().and()
                 .logout().permitAll().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).and()
                 .csrf().disable();
